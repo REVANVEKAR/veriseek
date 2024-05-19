d1ef52b85839202a254ac47979fcabfea6a5b19a
GeniusPlums <anish.adamane@gmail.com>
2024-05-19 16:50:58 +0530

    testing

--- lms/job/doctype/job_opportunity/job_opportunity.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2021, Frappe and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on("Job Opportunity", {
-	refresh: (frm) => {
-		if (frm.doc.name)
-			frm.add_web_link(`/job-openings/${frm.doc.name}`, "See on Website");
-	},
-});
--- lms/job/web_form/job_opportunity/job_opportunity.js
+++
@@ -1,7 +0,0 @@
-frappe.ready(function () {
-	frappe.web_form.after_save = () => {
-		setTimeout(() => {
-			window.location.href = `/job-openings`;
-		});
-	};
-});
